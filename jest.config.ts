6a5a36e9f420458aad1ae6d16d7a73a6d2d4c9f1
claudia-oldst <claudia@old.st>
2025-07-25 09:43:30 +0200

    libs - ui

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,11 @@
-import { getJestProjects } from "@nx/jest";
-
+/* eslint-disable */
 export default {
-    projects: getJestProjects(),
+    displayName: 'ui',
+    preset: '../../jest.preset.js',
+    transform: {
+        '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+        '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    coverageDirectory: '../../coverage/libs/ui',
 };
